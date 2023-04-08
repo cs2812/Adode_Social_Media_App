2dfbf9946ba4851c23399d585e629f6d24f04c9d
cs2812 <cs786786786@gmail.com>
2023-04-09 02:53:46 +0530

    setup client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <Box className="App" w="100%">
+
+    </Box>
+  );
+}
+
+export default App;
